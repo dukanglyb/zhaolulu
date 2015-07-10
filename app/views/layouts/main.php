602c626425b0501809e0fbda0e47c38a99934509
李杜康 <572609083@qq.com>
2015-07-10 15:20:49 +0800

    导航条

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -1,46 +1,48 @@
-<nav class="navbar navbar-default navbar-inverse" role="navigation">
-    <div class="container-fluid">
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+    <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">切换导航</span>
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">璐璐源</a>
+            <a href="javascript:void(0);" class="navbar-brand">璐璐源</a>
         </div>
         <?php echo $this->elements->getMenu(); ?>
     </div>
-</nav>
+</header>
 
-<div>
+
+<div class="zll-body">
     <?php echo $this->getContent(); ?>
     <hr>
     <div id="index-footer" class="footer">
         <div class="footer-center"
-            <!--合作伙伴-->
-            <div class="footer-link">
-                <p>
-                    <a href="/about/index">关于我们</a>
-                    <b>|</b>
-                    <a href="/contact/contactus">联系我们</a>
-                    <b>|</b>
-                    <a href="/contact/employ">招贤纳士</a>
-                    <b>|</b>
-                    <a href="/contact/suggestion">意见反馈</a>
-                </p>
-            </div>
-            <div class="footer-copyright">
-                <p>
-                    <em>Company &copy; 2014-<?php echo date("Y");?> 璐璐源 京 ICP 备 10011451号-6 北京璐璐源商贸有限公司</em>
-                </p>
-                <p style="text-align: center;">
-                    全国统一客服专线:18612098866    QQ在线技术咨询号码:245012628
-                </p>
-            </div>
+        <!--合作伙伴-->
+        <div class="footer-link">
+            <p>
+                <a href="/about/index">关于我们</a>
+                <b>|</b>
+                <a href="/contact/contactus">联系我们</a>
+                <b>|</b>
+                <a href="/contact/employ">招贤纳士</a>
+                <b>|</b>
+                <a href="/contact/suggestion">意见反馈</a>
+            </p>
+        </div>
+        <div class="footer-copyright">
+            <p>
+                <em>Company &copy; 2014-<?php echo date("Y"); ?> 璐璐源 京 ICP 备 10011451号-6 北京璐璐源商贸有限公司</em>
+            </p>
+
+            <p style="text-align: center;">
+                全国统一客服专线:18612098866 QQ在线技术咨询号码:245012628
+            </p>
         </div>
     </div>
-    <footer>
-        <p> </p>
-    </footer>
+</div>
+<footer>
+    <p></p>
+</footer>
 </div>
